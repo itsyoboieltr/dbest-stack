0da2cbb1dcb175542a2202f4a5e32330f8958403
Norbert Elter <72046715+itsyoboieltr@users.noreply.github.com>
2023-12-03 13:20:35 +0100

    fix: production build
    will use node for building only until the issue with bun is resolved

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import solid from 'solid-start/vite';
-import bun from 'solid-start-bun';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [solid({ adapter: bun(), ssr: false })],
+  plugins: [solid({ ssr: false })],
 });
