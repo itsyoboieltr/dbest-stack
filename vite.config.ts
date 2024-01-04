fa1807c592564bf0f8355c2f02e1f98e5a74afa9
Norbert Elter <72046715+itsyoboieltr@users.noreply.github.com>
2024-01-04 21:44:16 +0100

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,10 @@ export default defineConfig({
     },
     ssr: false,
   },
+  ssr: {
+    noExternal:
+      process.env.NODE_ENV === 'development'
+        ? []
+        : ['elysia', 'memoirist', '@sinclair/typebox'],
+  },
 });
