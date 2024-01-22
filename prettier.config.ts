3781504fa887eaa758e41e96d7fb600765f5d31c
Norbert Elter <72046715+itsyoboieltr@users.noreply.github.com>
2024-01-22 14:08:06 +0100

    fix: prettier

--- prettier.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Options } from 'prettier';
-
-export default {
-  singleQuote: true,
-  jsxSingleQuote: true,
-  bracketSameLine: true,
-  plugins: ['prettier-plugin-tailwindcss'],
-} satisfies Options;
