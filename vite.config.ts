d281b377d10fdf243115daa2aa0ece42740e0582
Norbert Elter <72046715+itsyoboieltr@users.noreply.github.com>
2024-01-28 12:29:25 +0100

    fix: ssr noExternal
    elysia bundling was fixed

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,4 @@ import { defineConfig } from '@solidjs/start/config';
 
 export default defineConfig({
   start: { server: { preset: 'bun' }, ssr: false },
-  ssr: {
-    noExternal:
-      process.env.NODE_ENV === 'development'
-        ? []
-        : ['elysia', 'memoirist', '@sinclair/typebox'],
-  },
 });
